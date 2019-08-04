2358e4be091149e8079d75342deb8d7bd8d1a628
Praveen <Praveen@DESKTOP-HK5J1E9>
2019-08-04 16:23:02 +0530

    Html helpers

---
+++ SampleCalculator/SampleCalculator/Controllers/SampleController.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace SampleCalculator.Controllers
+{
+    public class SampleController : Controller
+    {
+        // GET: Sample
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult Sample()
+        {
+            return View();
+        }
+    }
+}
