fac87a23b04b3cc8038fb5cb0f62d88dd6d438c2
ALS2422 <53687334+ALS2422@users.noreply.github.com>
2019-08-04 14:41:49 +0530

    Update Program.cs

--- SampleCalculator/SampleConsole/Program.cs
+++ SampleCalculator/SampleConsole/Program.cs
@@ -10,6 +10,7 @@ namespace SampleConsole
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("GitHUB conn");       }
+            Console.WriteLine("GitHUB conn");    
+        }
     }
 }
